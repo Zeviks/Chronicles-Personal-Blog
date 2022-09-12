aaa5ba0a7554a6c4bd2b61b2d120c1c7d0987084
Natanael Pacheco <natepacheco716@gmail.com>
2022-09-12 11:29:58 -0500

    Change colors for comment section.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -42,14 +42,14 @@ function Post({ post }: Props) {
 
   return <main>
       <Header />
-      <img className="w-full h-40 object-cover" src={urlFor(post.mainImage).url()!} alt=""/>
+      <img className="w-full h-56 object-cover" src={urlFor(post.mainImage).url()!} alt=""/>
       <article className="max-w-3xl mx-auto p-5">
         <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
         <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
 
         <div className="flex items-center space-x-2">
             <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()!} alt=""/>
-            <p className="font-extralight text-sm">Blog post by <span className="text-green-600">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
+            <p className="font-extralight text-sm">Blog post by <span className="text-briGreen">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}</p>
         </div>
 
         <div className="mt-10">
@@ -79,16 +79,16 @@ function Post({ post }: Props) {
             />
         </div>
       </article>
-      <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+      <hr className="max-w-lg my-5 mx-auto border border-briGreen" />
 
     {submitted ? (
-        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+        <div className="flex flex-col p-10 my-10 bg-briGreen text-white max-w-2xl mx-auto">
             <h3 className="text-3xl font-bold">Thank you for submitting your comment!</h3>
             <p>Once it has been approved, it will appear below!</p>
         </div>
     ):(
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
-        <h3 className="text-sm text-yellow-500">Enjoye this article?</h3>
+        <h3 className="text-sm text-BriGreen">Enjoye this article?</h3>
         <h4 className="text-3xl font-bold">Leave a comment below!</h4>
         <hr className="py-3 mt-2" />
 
@@ -129,17 +129,17 @@ function Post({ post }: Props) {
         )}
        </div>
 
-       <input type="submit" className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" />
+       <input type="submit" className="shadow bg-briGreen hover:bg-fontColor focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded transition-all cursor-pointer" />
       </form>
     )}
 
-    <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+    <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-briGreen shadow space-y-2">
         <h3 className="text-4xl">Comments</h3>
         <hr />
         {post.comments.map((comment) => (
             <div key={comment._id}>
                 <p>
-                    <span className="text-yellow-500">{comment.name}: </span>{comment.comment}
+                    <span className="text-BriGreen">{comment.name}: </span>{comment.comment}
                 </p>
             </div>
         ))}
